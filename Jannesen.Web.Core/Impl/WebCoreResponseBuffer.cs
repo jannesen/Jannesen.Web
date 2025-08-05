72e67a11ec95500f66019f16b7a80602b83c3aa8
Peter Jannesen <peter@jannesen.com>
2025-08-05 12:59:42 +0200

    Bugfix logging

--- Jannesen.Web.Core/Impl/WebCoreResponseBuffer.cs
+++ Jannesen.Web.Core/Impl/WebCoreResponseBuffer.cs
@@ -203,14 +203,16 @@ namespace Jannesen.Web.Core.Impl
 
         public      override    void                WriteLoggingData(StreamWriter writer)
         {
-            if (_contentType.IndexOf("charset=utf-8", StringComparison.Ordinal) > 0) {
-                writer.WriteLine();
-                writer.Flush();
-                writer.BaseStream.Write(_data, 0, _length);
-                writer.WriteLine();
+            if (_data != null) {
+                if (_contentType.IndexOf("charset=utf-8", StringComparison.Ordinal) > 0) {
+                    writer.WriteLine();
+                    writer.Flush();
+                    writer.BaseStream.Write(_data, 0, _length);
+                    writer.WriteLine();
+                }
+                else
+                    writer.WriteLine("[BINARY-DATA]");
             }
-            else
-                writer.WriteLine("[BINARY-DATA]");
         }
     }
 }
